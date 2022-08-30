402df450c4c7e5880e7711371bcee86a5a326391
Tyler Vezina <tylerjvezina@gmail.com>
2022-08-30 00:30:01 -0400

    Updated asset loading functions to use and enforce a certain folder structure

--- asset-loading.ts
+++ asset-loading.ts
@@ -6,19 +6,19 @@ type LoadFunc<T> = (path: string, onComplete?: LoadCallback<T>) => void;
  * optional callback to allow awaiting assets sequentially or simultaneously.
  *
  * Example 1 - in sequence:
- *    myImage1 = await loadImageAsync('path/to/asset/1.png');
- *    myImage2 = await loadImageAsync('path/to/asset/2.png');
+ *    myImage1 = await loadImageAsync('img1.png');
+ *    myImage2 = await loadImageAsync('img2.png');
  *
  * Example 2 - simultaneous w/return:
  *    [myImage1, myImage2] = await Promise.all([
- *      loadImageAsync('path/to/asset/1.png'),
- *      loadImageAsync('path/to/asset/2.png'),
+ *      loadImageAsync('img1.png'),
+ *      loadImageAsync('img2.png'),
  *    ]);
  *
  * Example 3 - simultaneous w/callbacks:
  *    await Promise.all([
- *      loadImageAsync('path/to/asset/1.png', img => { myImage1 = img; }),
- *      loadImageAsync('path/to/asset/2.png', img => { myImage2 = img; }),
+ *      loadImageAsync('img1.png', img => { myImage1 = img; }),
+ *      loadImageAsync('img2.png', img => { myImage2 = img; }),
  *    ]);
  */
 function makeLoadFuncAsync<T>(loadFunc: LoadFunc<T>, path: string, onComplete?: LoadCallback<T>): Promise<T> {
@@ -28,8 +28,20 @@ function makeLoadFuncAsync<T>(loadFunc: LoadFunc<T>, path: string, onComplete?: 
   }));
 }
 
-export function loadImageAsync(path: string, onComplete?: LoadCallback<p5.Image>): Promise<p5.Image> {
-  return makeLoadFuncAsync<p5.Image>(loadImage, path, onComplete);
+export function loadImageAsync(fileName: string, onComplete?: LoadCallback<p5.Image>): Promise<p5.Image> {
+  return makeLoadFuncAsync<p5.Image>(loadImage, `./assets/images/${fileName}`, onComplete);
+}
+
+export function loadSoundAsync(filePath: string, onComplete?: LoadCallback<p5.SoundFile>): Promise<p5.SoundFile> {
+  return makeLoadFuncAsync<p5.SoundFile>(loadSound, `./assets/audio/${filePath}`, onComplete);
+}
+
+export function loadFontAsync(fileName: string, onComplete?: LoadCallback<p5.Font>): Promise<p5.Font> {
+  return makeLoadFuncAsync<p5.Font>(loadFont, `./assets/fonts/${fileName}`, onComplete);
+}
+
+export function loadModelAsync(fileName: string, onComplete?: LoadCallback<p5.Geometry>): Promise<p5.Geometry> {
+  return makeLoadFuncAsync<p5.Geometry>(loadModel, `./assets/models/${fileName}`, onComplete);
 }
 
 export function loadStringsAsync(path: string, onComplete?: LoadCallback<string[]>): Promise<string[]> {
@@ -38,16 +50,4 @@ export function loadStringsAsync(path: string, onComplete?: LoadCallback<string[
 
 export function loadJSONAsync(path: string, onComplete?: LoadCallback<object>): Promise<object> {
   return makeLoadFuncAsync<object>(loadJSON, path, onComplete);
-}
-
-export function loadFontAsync(path: string, onComplete?: LoadCallback<p5.Font>): Promise<p5.Font> {
-  return makeLoadFuncAsync<p5.Font>(loadFont, path, onComplete);
-}
-
-export function loadSoundAsync(path: string, onComplete?: LoadCallback<p5.SoundFile>): Promise<p5.SoundFile> {
-  return makeLoadFuncAsync<p5.SoundFile>(loadSound, path, onComplete);
-}
-
-export function loadModelAsync(path: string, onComplete?: LoadCallback<p5.Geometry>): Promise<p5.Geometry> {
-  return makeLoadFuncAsync<p5.Geometry>(loadModel, path, onComplete);
 }
